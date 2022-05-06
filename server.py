6e197b28833db36e4414577d3061f0713a6384a0
Campeon Campeon <campeon@protonmail.com>
2022-05-05 22:21:40 -0500

    My portfolio project.
    - Initial commit.

---
+++ server.py
@@ -0,0 +1,57 @@
+import os
+import csv
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database_csv:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            database_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    error = None
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database.'
+    else:
+        error = 'Something went wrong. Try again!'
+        return error
+    #     if valid_login(request.form['username'],
+    #                    request.form['password']):
+    #         return log_the_user_in(request.form['username'])
+    #     else:
+    #         error = 'Invalid username/password'
+
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    # return render_template('login.html', error=error)
